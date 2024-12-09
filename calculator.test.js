8c4ed911edb81102721915b314735c24b71aa91a
Dhwani-Joshi <dhwanijoshi44@gmail.com>
2024-12-09 20:20:05 +0530

    Add Test for Multiple Numbers

--- calculator.test.js
+++ calculator.test.js
@@ -1,10 +1,15 @@
 const { add } = require('./calculator');
 
 test('empty string returns 0', () => {
-  expect(add('')).toBe(0);
+    expect(add('')).toBe(0);
 });
 
 test('single number returns the same number', () => {
     expect(add('1')).toBe(1);
-  });
-  
+});
+
+test('multiple numbers are summed correctly', () => {
+    expect(add('1,5')).toBe(6);
+    expect(add('1,2,3')).toBe(6);
+    expect(add('4,5,6,7')).toBe(22);
+});
