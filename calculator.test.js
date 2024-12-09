b4a456fe54db2bd4516b591c192d992bc66197fb
Dhwani-Joshi <dhwanijoshi44@gmail.com>
2024-12-09 20:12:20 +0530

    Add Test for One Number

--- calculator.test.js
+++ calculator.test.js
@@ -3,3 +3,8 @@ const { add } = require('./calculator');
 test('empty string returns 0', () => {
   expect(add('')).toBe(0);
 });
+
+test('single number returns the same number', () => {
+    expect(add('1')).toBe(1);
+  });
+  
