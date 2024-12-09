770f0c7a32721e07098de4a140c3c25a5fc1c029
Dhwani-Joshi <dhwanijoshi44@gmail.com>
2024-12-09 20:23:15 +0530

    Support Newlines Between Numbers

--- calculator.js
+++ calculator.js
@@ -1,6 +1,8 @@
 function add(numbers) {
     if (numbers === "") return 0;
-    return numbers.split(',').reduce((sum, num) => sum + Number(num), 0);
+    return numbers.split(/[\n,]/).reduce((sum, num) => sum + Number(num), 0);
+    // return numbers.split(',').reduce((sum, num) => sum + Number(num), 0);
+
   }
   
   module.exports = { add };
--- calculator.test.js
+++ calculator.test.js
@@ -13,3 +13,9 @@ test('multiple numbers are summed correctly', () => {
     expect(add('1,2,3')).toBe(6);
     expect(add('4,5,6,7')).toBe(22);
 });
+
+test('newlines are treated as delimiters', () => {
+    expect(add('1\n2,3')).toBe(6);
+    expect(add('4\n5\n6')).toBe(15);
+  });
+  
